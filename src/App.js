125f60fa73dcab1bbded56aee7f5a967f4e80004
antenorq <antenorq@gmail.com>
2023-01-13 01:59:24 -0500

    Initial tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
+      <Test/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
