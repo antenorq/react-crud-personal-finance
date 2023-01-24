e3fd2ab5e894ef9ef70cf05b17262923a137513f
igloo400 <antenor@igloo400.com>
2023-01-24 17:05:21 -0500

    created income and expense component

---
+++ src/components/Income.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import 
+
+const Income = () => {
+  const { category, setcategory } = useState("");
+  const { description, setdescription } = useState("");
+  const { value, setValue } = useState("");
+  const { date, setDate } = useState("");
+
+  return <div>Income</div>;
+};
+
+export default Income;
